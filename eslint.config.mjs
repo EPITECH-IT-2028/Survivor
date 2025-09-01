253f3b42e232cf746b388d2dfb06d5715882bed3
Quentin L <quentin.lapierre@epitech.eu>
2025-09-01 12:35:50 +0200

    feat(lint): add better-tailwindcss plugin to ESLint config
    Integrate eslint-plugin-better-tailwindcss for improved Tailwind CSS
    linting.
    Update ESLint configuration to enable and configure relevant rules for
    Tailwind CSS class order, variable syntax, and duplicate class
    detection.
    Add the plugin to dependencies in package.json and update
    package-lock.json.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import betterTailwindcss from "eslint-plugin-better-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -20,6 +20,32 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "better-tailwindcss": betterTailwindcss,
+    },
+    settings: {
+      "better-tailwindcss": {
+        "entryPoint": "app/globals.css",
+      }
+    },
+    rules: {
+      "better-tailwindcss/enforce-consistent-line-wrapping": "off",
+      "better-tailwindcss/enforce-consistent-class-order": "warn",
+      "better-tailwindcss/enforce-consistent-variable-syntax": "warn",
+      "better-tailwindcss/enforce-consistent-important-position": "warn",
+      "better-tailwindcss/enforce-shorthand-classes": "warn",
+      "better-tailwindcss/no-duplicate-classes": "error",
+      "better-tailwindcss/no-deprecated-classes": "error",
+      "better-tailwindcss/no-unnecessary-whitespace": "warn",
+
+      "better-tailwindcss/no-unregistered-classes": "off",
+      "better-tailwindcss/no-conflicting-classes": "error",
+      "better-tailwindcss/no-restricted-classes": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
+
