9c07161407ad1362d08daa85184bdc196abf9912
Quentin L <quentin.lapierre@epitech.eu>
2025-09-02 02:57:25 +0200

    feat(navbar): update layout and button styles
    Refactor the Navbar component to improve its layout and button styles.
    - Add new "Discover Startups" and "See Events" buttons with underline
    and
      hover effects.
    - Adjust spacing and typography for a cleaner look.
    - Update "Login" button styling for better contrast.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,18 @@
 export default function Navbar() {
   return (
     <nav className="mx-6 mt-5 flex h-24 items-center justify-between rounded-xl bg-primary px-16 font-instrument-serif text-secondary">
-      <div className="text-4xl">
-        The Incubator
-      </div>
-      <div className="flex items-center gap-4">
-        <button className="rounded-lg border-1 border-secondary px-6 py-2 text-lg font-medium text-secondary transition-transform duration-100 hover:scale-x-110 hover:scale-y-95 hover:bg-secondary hover:text-primary">
+      <div className="text-4xl">The Incubator.</div>
+        <div className="flex gap-6">
+          <button className="rounded-lg px-6 py-2 font-sf-pro text-lg font-medium text-secondary underline decoration-wavy decoration-1 underline-offset-4 transition-all duration-100 hover:scale-x-110 hover:scale-y-95 hover:decoration-zinc-400">
+            Discover Startups
+          </button>
+          <button className="rounded-lg px-6 py-2 font-sf-pro text-lg font-medium text-secondary underline decoration-wavy decoration-1 underline-offset-4 transition-all duration-100 hover:scale-x-110 hover:scale-y-95 hover:decoration-zinc-400">
+            See Events
+          </button>
+        </div>
+        <button className="rounded-lg border-1 border-secondary bg-secondary px-6 py-2 text-lg font-medium text-primary transition-transform duration-100 hover:scale-x-110 hover:scale-y-95 hover:bg-primary hover:text-secondary">
           Login
         </button>
-      </div>
     </nav>
   );
 }
