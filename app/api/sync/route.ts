c00780a85b51a638d187605f515ef71b534d5048
Nolann Dubos <nolann.dubos@epitech.eu>
2025-09-02 10:11:04 +0200

    feat: insert users into db

--- app/api/sync/route.ts
+++ app/api/sync/route.ts
@@ -4,7 +4,14 @@ import { neon } from '@neondatabase/serverless';
 async function fetchDataFromExternalAPI(endpoint: string) {
 
   const response = await Promise.all([
-    fetch(`https://api.jeb-incubator.com/${endpoint}`).then(res => res.json()),
+    fetch(`https://api.jeb-incubator.com/${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': ``,
+      },
+    }
+    ).then(res => res.json()),
   ]);
 
   return response
@@ -16,7 +23,21 @@ export async function POST(req: Request, res: NextResponse) {
 
     const response = await fetchDataFromExternalAPI('users');
 
-    console.log('Fetched data:', response);
+    if (!response) {
+      return NextResponse.json({ error: 'Failed to fetch data from external API' }, {
+        status: 500,
+      });
+    }
+    const users = response[0];
+
+    for (const user of users) {
+      await db.query(
+        `INSERT INTO users (name, email, role, founder_id, investor_id) VALUES ($1, $2, $3, $4, $5)
+         ON CONFLICT (email) DO UPDATE SET name = EXCLUDED.name,
+         role = EXCLUDED.role, founder_id = EXCLUDED.founder_id, investor_id = EXCLUDED.investor_id`,
+        [user.name, user.email, user.role, user.founder_id, user.investor_id]
+      );
+    }
 
     return NextResponse.json({ message: 'Data synchronized successfully' });
   } catch (error) {
