bb3dd873570d1094b2e1e9725ee8f5592551a8f4
Nolann Dubos <nolann.dubos@epitech.eu>
2025-09-02 20:03:29 +0200

    fix: session.ts types

--- app/hooks/session/session.ts
+++ app/hooks/session/session.ts
@@ -1,11 +1,11 @@
 import 'server-only'
-import { SignJWT, jwtVerify } from 'jose'
+import { JWTPayload, SignJWT, jwtVerify } from 'jose'
 import { cookies } from 'next/headers'
 
 const secretKey = process.env.SESSION_SECRET
 const encodedKey = new TextEncoder().encode(secretKey)
 
-export async function encrypt(payload: any) {
+export async function encrypt(payload: JWTPayload) {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
@@ -24,7 +24,7 @@ export async function decrypt(session: string | undefined = '') {
   }
 }
 
-export async function createSession(userId: any) {
+export async function createSession(userId: string) {
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
   const session = await encrypt({ userId, expiresAt })
   const cookieStore = await cookies()
