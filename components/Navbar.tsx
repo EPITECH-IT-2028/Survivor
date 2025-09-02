f045019c48d88aa37409fdcefc6a7419d1f7215d
Quentin L <quentin.lapierre@epitech.eu>
2025-09-02 15:56:43 +0200

    feat(navbar): update layout and styles for improved UX
    Refactored the Navbar component to enhance layout and visual hierarchy:
    - Made the navbar position relative and adjusted font family.
    - Positioned the logo absolutely to the left.
    - Centered navigation links and updated their hover effects.
    - Moved the Login button to the absolute right and refined its styling.
    These changes improve navigation clarity and overall user experience.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,26 +5,26 @@ export default function Navbar() {
   return (
     <nav
       aria-label="Navigation principale"
-      className="mx-6 mt-5 flex h-24 items-center justify-between rounded-xl bg-primary px-16 font-instrument-serif text-secondary"
+      className="relative mx-6 mt-5 flex h-24 items-center justify-between rounded-xl bg-primary font-canela-black text-secondary"
     >
-      <TextRipple className="text-4xl">JEB Incubator.</TextRipple>
-      <div className="flex gap-6">
+      <TextRipple className="absolute left-6 pt-2 text-4xl">JEB Incubator.</TextRipple>
+      <div className="mx-auto">
         <Link
           href="#"
-          className="rounded-lg px-6 py-2 font-sf-pro text-lg font-medium text-secondary underline decoration-wavy decoration-1 underline-offset-4 transition-all duration-100 hover:scale-x-110 hover:scale-y-95 hover:decoration-zinc-400"
+          className="mx-6 font-sf-pro text-lg font-medium text-secondary underline decoration-wavy decoration-1 underline-offset-4 transition-all duration-100 hover:scale-x-110 hover:scale-y-95 hover:text-zinc-400 hover:decoration-zinc-400"
         >
           Discover Startups
         </Link>
         <Link
           href="#"
-          className="rounded-lg px-6 py-2 font-sf-pro text-lg font-medium text-secondary underline decoration-wavy decoration-1 underline-offset-4 transition-all duration-100 hover:scale-x-110 hover:scale-y-95 hover:decoration-zinc-400"
+          className="mx-6 font-sf-pro text-lg font-medium text-secondary underline decoration-wavy decoration-1 underline-offset-4 transition-all duration-100 hover:scale-x-110 hover:scale-y-95 hover:text-zinc-400 hover:decoration-zinc-400"
         >
           See Events
         </Link>
       </div>
       <Link
         href="#"
-        className="rounded-lg border border-secondary bg-secondary px-6 py-2 text-lg font-medium text-primary transition-transform duration-100 hover:scale-x-110 hover:scale-y-95 hover:bg-primary hover:text-secondary"
+        className="absolute right-0 mr-6 rounded-lg border border-secondary bg-secondary px-6 py-2 text-lg font-medium text-primary transition-transform duration-100 hover:scale-x-110 hover:scale-y-95 hover:bg-primary hover:text-secondary"
       >
         Login
       </Link>
