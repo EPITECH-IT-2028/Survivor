446b809ce9a864b098b1474125070828f9cdeebe
Nolann Dubos <nolann.dubos@epitech.eu>
2025-09-03 15:59:04 +0200

    feat: begin to implement CRUD for the DB

---
+++ app/api/events/[id]/route.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { getSql } from "@/lib/db";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const db = getSql();
+
+  if (db === null) {
+    return new Response(JSON.stringify({ error: 'Database connection failed' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  const { id } = await params;
+
+  try {
+    const response = await db`SELECT * FROM events WHERE id = ${id}`;
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: 'Failed to fetch events' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
---
+++ app/api/users/route.ts
@@ -0,0 +1,26 @@
+'use server';
+import { getSql } from "@/lib/db";
+
+export async function GET() {
+  const db = getSql();
+
+  if (db === null) {
+    return new Response(JSON.stringify({ error: 'Database connection failed' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  try {
+    const response = await db`SELECT * FROM users`;
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: 'Failed to fetch users' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
