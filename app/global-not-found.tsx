fe7964439be0f8cf07054435579f625fe403cace
ZifFiji <arthur.guerinault@epitech.eu>
2025-09-05 10:35:30 +0200

    fix: remove useless fallback font and update a tag to link next tag

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -2,24 +2,13 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Navbar from '@/components/Navbar'
 import { AuthProvider } from '@/lib/auth-context'
-import { Geist, Geist_Mono } from "next/font/google";
-import { Instrument_Serif } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import { Button } from '@/components/ui/button';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Link from 'next/link';
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
-
-const instrumentSerif = Instrument_Serif({
-  variable: "--font-instrument-serif",
-  subsets: ["latin"],
-  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -31,7 +20,7 @@ export default function GlobalNotFound() {
   return (
     <html lang="en">
       <body
-        className={`font-sf-pro ${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} antialiased`}
+        className={`font-sf-pro ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
           <Navbar />
@@ -41,9 +30,9 @@ export default function GlobalNotFound() {
               <p className="text-md">
                 The page you try to access is not available, please go back to home
               </p>
-              <a href='/'>
+              <Link href='/'>
                 <Button variant="outline">Return to main page</Button>
-              </a>
+              </Link>
             </div>
           </div>
         </AuthProvider>
