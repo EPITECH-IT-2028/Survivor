4dd1391f2ed54cde222793bf01d921860a91dd7c
Quentin L <quentin.lapierre@epitech.eu>
2025-09-11 11:48:34 +0200

    feat(ui): improve EventCard layout and image style
    Refactor EventCard to use a flex column layout, ensuring better
    alignment
    and spacing of event details and image. Adjust EventImage to use rounded
    corners for a more consistent appearance. These changes enhance the
    visual presentation and readability of event cards in the event
    calendar.

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -15,35 +15,39 @@ export default function EventCard({ event }: { event: TEvent }) {
   };
 
   return (
-    <Card className="h-[24rem] w-full cursor-pointer rounded-xl bg-white">
+    <Card className="flex h-[24rem] w-full cursor-pointer flex-col rounded-xl bg-white">
       <CardHeader>
         <CardTitle className="line-clamp-2 text-lg font-bold">
           {event.name}
         </CardTitle>
       </CardHeader>
-      <CardContent>
-        <div className="flex items-center">
-          <span className="w-16 text-xs font-medium text-gray-500">Date:</span>
-          <span className="text-sm text-gray-700">
-            {event.dates ? formatDate(event.dates) : "N/A"}
-          </span>
-        </div>
-        <div className="flex items-center">
-          <span className="w-16 text-xs font-medium text-gray-500">
-            Location:
-          </span>
-          <span className="truncate text-sm text-gray-700">
-            {event.location || "N/A"}
-          </span>
-        </div>
-        {event.description && (
-          <div className="mt-3">
-            <p className="line-clamp-3 text-xs text-gray-600">
-              {event.description}
-            </p>
+      <CardContent className="flex flex-grow flex-col">
+        <div className="flex-grow">
+          <div className="flex items-center">
+            <span className="w-16 text-xs font-medium text-gray-500">
+              Date:
+            </span>
+            <span className="text-sm text-gray-700">
+              {event.dates ? formatDate(event.dates) : "N/A"}
+            </span>
           </div>
-        )}
-        <div className="mt-3">
+          <div className="flex items-center">
+            <span className="w-16 text-xs font-medium text-gray-500">
+              Location:
+            </span>
+            <span className="truncate text-sm text-gray-700">
+              {event.location || "N/A"}
+            </span>
+          </div>
+          {event.description && (
+            <div className="mt-3">
+              <p className="line-clamp-3 text-xs text-gray-600">
+                {event.description}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="mt-4">
           <EventImage id={event.id} />
         </div>
       </CardContent>
