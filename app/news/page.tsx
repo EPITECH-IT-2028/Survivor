b47fdceb433aaaa47574d43beb53ffadba778914
Quentin L <quentin.lapierre@epitech.eu>
2025-09-11 11:14:50 +0200

    refactor(news): improve NewsCard layout and image display
    - Updated NewsCard grid layout for better responsiveness.
    - Simplified and improved the card content structure for clarity.
    - Enhanced NewsImage component to use aspect ratio and object-cover for
      better image presentation.
    - Adjusted date formatting and removed unused time fields.
    - Improved visual alignment and spacing for card details.

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -21,12 +21,12 @@ export default function News() {
   return (
     <div className="relative left-1/2 flex w-dvw -translate-x-1/2 justify-center space-y-8 p-6">
       {newsInfo.length === 0 && (
-        <div className="flex flex-col justify-center items-center min-h-screen">
+        <div className="flex min-h-screen flex-col items-center justify-center">
           <PulseLoader size={30} color="#F18585" />
         </div>
       )}
       <div>
-        <div className="grid w-full max-w-6xl grid-cols-2 gap-6 md:grid-cols-1 lg:grid-cols-2">
+        <div className="grid w-full max-w-6xl grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {newsInfo.map((news) => (
               <NewsCard key={news.id} news={news} />
             ))}
--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -19,8 +19,6 @@ export default function NewsCard({ news }: { news: TNews }) {
       year: "numeric",
       month: "long",
       day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
     });
   };
 
